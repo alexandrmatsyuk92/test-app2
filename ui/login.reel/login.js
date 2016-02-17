34a123245a50707954fbb1019edae40e8b88c053
Alexandr Matsyuk <noreply>
2016-02-17 20:49:42 +0000

    Update files

--- ui/login.reel/login.js
+++ ui/login.reel/login.js
@@ -3,7 +3,7 @@
  */
 var Component = require("montage/ui/component").Component;
 var sharedAuthService = require("core/auth-service").shared;
-
+var $ = require('jquery-latest');
 
 /**
  * @class Login
@@ -19,7 +19,29 @@ exports.Login = Component.specialize(/** @lends Login# */ {
     handleButtonLoginAction: {
         value: function (ev) {
             var _this = this;
+            $.ajax({
+                url: "mydomain.com/url",
+                type: "POST",
+                /*contentType: "application/json",*/
+                data: {email: _this.email, password: _this.password},
 
+
+                success: function(data) {
+                    cosole.log(data);
+                    //called when successful
+                },
+
+                error: function(err) {
+                    console.log(err);
+                    //called when there is an error
+                },
+            });
+                    /*sharedAuthService.setToken(resp.token);
+                    //_this.templateObjects.router.updatePath('landing');
+                    var event = document.createEvent("CustomEvent");
+                    event.initCustomEvent('signin', true, true, null);
+                    _this.dispatchEvent(event);*/
+            
         }
     }
 
